f3e3c8aeeb8e31bedebcb781c552b90336766fa9
Violetta <94271862+burlakova-veta@users.noreply.github.com>
2023-05-11 20:44:27 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace csharptest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите целое число");
+            int fib0 = 1;
+            int fib1 = 1;
+            Console.Write(fib0 + " " + fib1 + " ");
+            for (int i = 3; i <= n; i++)
+            {
+                int c = fib1;
+                fib1 = fib1 + fib0;
+                fib0 = c;
+                Console.Write(fib1 + " ");
+            }
+        }
+    }
+}
